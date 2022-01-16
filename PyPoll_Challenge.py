3c6a997a3be1848fcb27f2c3e89b7d23d4ad248c
Nishat Sultana <65102723+NishatSultana3538@users.noreply.github.com>
2022-01-16 01:18:22 -0500

    upload 1.16.22

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -137,7 +137,7 @@ with open(file_to_save, "w") as txt_file:
 
     # 8: Save the county with the largest turnout to a text file.
 
-    txt_file.write(winning_county)
+    txt_file.write(largest_county_voter_turnout)
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
 
